8bb00e51fd126d770b8521be36e4107c0ed36d7f
Chivano295 <crijsdam@hotmail.com>
2022-01-20 08:38:40 +0100

    tmp + pauze scherm

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
